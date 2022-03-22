e053045aca3ff8433bd4f9ac4af670b979059ea0
Vladimir <39942321+Vlad1m8r@users.noreply.github.com>
2022-03-22 13:07:45 +0300

    master (start)

--- src/App/App.js
+++ src/App/App.js
@@ -21,7 +21,7 @@ const App = () => {
 
     return (
 
-        <BrowserRouter>
+        <BrowserRouter basename={"http://dayhome.ru"}>
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
